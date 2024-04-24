ca601776d04f37a09c367ab0afba289a377e09e8
Dirk Thayer <dbt5b1fd@lcstudent.org>
2024-04-24 14:42:21 -0600

    Removed unneeded code.

--- Core/src/robot-config.cpp
+++ Core/src/robot-config.cpp
@@ -3,8 +3,6 @@
 #include "robot-config.h"
 
 using namespace vex;
-using signature = vision::signature;
-using code = vision::code;
 
 // A global instance of brain used for printing to the V5 brain screen
 brain Brain;
